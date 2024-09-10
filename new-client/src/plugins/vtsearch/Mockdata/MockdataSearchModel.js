edda7ee4b32fd73d1b65473ced26cb47e7ecb01f
Martin Arvroth Thorbjörnsson <martin.thorbjornsson@sweco.se>
2024-09-10 16:34:35 +0200

    Removed backend and new-client folders from root

--- new-client/src/plugins/vtsearch/Mockdata/MockdataSearchModel.js
+++
@@ -1,9 +0,0 @@
-export const MockdataSearchModel = () => {
-  return {
-    municipalities: [
-      { name: "", gid: null },
-      { name: "Ale", gid: "9081014110001440" },
-    ],
-    modeTypeNames: ["", "Buss"],
-  };
-};
