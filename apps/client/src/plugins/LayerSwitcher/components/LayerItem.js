7411926e5039c62576f603dccfd14962a1e4eeeb
Alex Stendahl <alex.stendahl@sweco.se>
2024-10-02 10:53:35 +0200

    GEOU-2655: Code cleanup

--- apps/client/src/plugins/LayerSwitcher/components/LayerItem.js
+++ apps/client/src/plugins/LayerSwitcher/components/LayerItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { withSnackbar } from "notistack";
 import { styled } from "@mui/material/styles";
 import { Button, Typography, Grid, Link } from "@mui/material";
-import { IconButton, Paper } from "@mui/material";
+import { IconButton } from "@mui/material";
 import IconWarning from "@mui/icons-material/Warning";
 import CallMadeIcon from "@mui/icons-material/CallMade";
 import InfoIcon from "@mui/icons-material/Info";
